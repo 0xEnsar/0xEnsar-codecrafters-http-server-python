3c071725418bff8baeca569791d11a842988a70b
0xEnsar <ensaryozgat@gmail.com>
2024-05-19 23:01:39 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
-# Uncomment this to pass the first stage
 import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    conn, addr = server_socket.accept()
+
+    print("Connection from: ", addr)
+
 
 
 if __name__ == "__main__":
