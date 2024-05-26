361f07428530349442a8407585192ba427d66f75
0xEnsar <ensaryozgat@gmail.com>
2024-05-26 14:08:00 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,37 +1,45 @@
 import socket
+import threading
 
-
-def main():
-
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
-    conn, addr = server_socket.accept()
-
+def connectionHandler(conn, addr):
     print("Connection from: ", addr)
 
     data = conn.recv(1024).decode("utf-8")
     print("Data: ", data)
 
     path = data.split()[1]
-    
+
     if path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n"
         conn.send(response.encode())
-    
+
     elif path.startswith("/echo"):
-        echoPath = path[6:]
-        print("Echo path: ", echoPath)
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echoPath)}\r\n\r\n{echoPath}\r\n"
+        echo_path = path[6:]
+        print("Echo path: ", echo_path)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_path)}\r\n\r\n{echo_path}\r\n"
         conn.send(response.encode())
 
     elif path.startswith("/user-agent"):
-        userAgentPath = data.split("User-Agent: ")[1].split("\r\n")[0]
-        print("User-Agent path: ", userAgentPath)
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(userAgentPath)}\r\n\r\n{userAgentPath}\r\n"
+        user_agent_path = data.split("User-Agent: ")[1].split("\r\n")[0]
+        print("User-Agent path: ", user_agent_path)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent_path)}\r\n\r\n{user_agent_path}\r\n"
         conn.send(response.encode())
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
         conn.send(response.encode())
+
+
+
+
+def main():
+
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.listen()
+    while True:
+        conn, addr = server_socket.accept()
+        threading.Thread(target= connectionHandler, args=(conn, addr)).start()
+    
+   
 
 
 
